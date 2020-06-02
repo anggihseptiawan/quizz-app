7fe142b4b01f4dbc084b942f45341cdc78eb295f
Anggih Septiawan <albertanggih@gmail.com>
2020-06-02 14:00:02 +0700

    first commit, start making question component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+const COMP_URL =
+	"https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [questions, setQuestion] = useState([]);
+
+	useEffect(() => {
+		fetch(COMP_URL)
+			.then((res) => res.json())
+			.then((data) => setQuestion(data.results));
+	}, []);
+
+	if (questions.length > 0) {
+		return (
+			<div className="container">
+				<h5
+					className="question text-2xl"
+					dangerouslySetInnerHTML={{ __html: questions[0].question }}
+				/>
+
+				<div className="grid grid-cols-2 gap-3">
+					<button
+						className="item p-4"
+						dangerouslySetInnerHTML={{
+							__html: questions[0].correct_answer,
+						}}
+					/>
+					<button
+						className="item p-4"
+						dangerouslySetInnerHTML={{
+							__html: questions[0].incorrect_answers[0],
+						}}
+					/>
+					<button
+						className="item p-4"
+						dangerouslySetInnerHTML={{
+							__html: questions[0].incorrect_answers[1],
+						}}
+					/>
+					<button
+						className="item p-4"
+						dangerouslySetInnerHTML={{
+							__html: questions[0].incorrect_answers[2],
+						}}
+					/>
+				</div>
+			</div>
+		);
+	} else {
+		return "waiting for question";
+	}
 }
 
 export default App;
